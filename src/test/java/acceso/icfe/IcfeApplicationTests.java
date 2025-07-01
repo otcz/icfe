286e9b13d3618b70f1c3f824206f12074e3c09be
COLSOAT <carrillozuletaoscar@gmail.com>
2025-07-01 11:33:49 -0500

    Commit init

---
+++ src/test/java/acceso/icfe/IcfeApplicationTests.java
@@ -0,0 +1,13 @@
+package acceso.icfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
